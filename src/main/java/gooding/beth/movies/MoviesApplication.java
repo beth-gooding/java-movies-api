a4780b8773cd07dcb18120e09b3e6b1997a19c69
beth-gooding <beth.gooding@hotmail.com>
2023-04-13 14:37:45 +0100

    feat: Adds test endpoint

--- src/main/java/gooding/beth/movies/MoviesApplication.java
+++ src/main/java/gooding/beth/movies/MoviesApplication.java
@@ -1,13 +1,20 @@
-package gooding.beth.movies;
+package gooding.beth.movies; // Declare our package
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication; // Contains the run method that will start the spring application
+import org.springframework.boot.autoconfigure.SpringBootApplication; // Imports the annotation used on Line 6
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MoviesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesApplication.class, args);
 	}
 
+	@GetMapping("/root") // Lets the framework know that this endpoint is a get endpoint
+	public String apiRoot() {
+		return "Hello World!";
+	}
 }
